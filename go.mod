938d4337dc5eb6a81ea3e4775f06928193b341f9
junyaUch <junya19991215@icloud.com>
2021-01-03 16:10:18 +0900

    環境の設定

---
+++ go.mod
@@ -0,0 +1,8 @@
+module NativeAppApi
+
+go 1.15
+
+require (
+	github.com/astaxie/beego v1.12.3
+	github.com/go-sql-driver/mysql v1.5.0
+)
--- main.go
+++ main.go
@@ -1,15 +1,53 @@
 package main
 
 import (
+	"NativeAppApi/models"
 	_ "NativeAppApi/routers"
+	"fmt"
 
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
+	"github.com/astaxie/beego/plugins/cors"
+	"github.com/astaxie/beego/session"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
-	if beego.BConfig.RunMode == "dev" {
-		beego.BConfig.WebConfig.DirectoryIndex = true
-		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+	setupDB()
+
+	sessionconf := &session.ManagerConfig{
+		CookieName:      "NativeID",
+		Gclifetime:      3600,
+		EnableSetCookie: true,
+		Maxlifetime:     3600,
+		Secure:          true,
+		CookieLifeTime:  3600,
+		ProviderConfig:  "",
 	}
+	beego.GlobalSessions, _ = session.NewManager("memory", sessionconf)
+	go beego.GlobalSessions.GC()
+
+	beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "DELETE", "PUT", "PATCH", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Access-Control-Allow-Origin"},
+		ExposeHeaders:    []string{"Content-Length", "Access-Control-Allow-Origin"},
+		AllowCredentials: true,
+	}))
+
 	beego.Run()
+}
+
+func setupDB() {
+	db := beego.AppConfig.String("driver")
+
+	orm.RegisterDriver(db, orm.DRMySQL)
+	orm.RegisterDataBase("default", db, beego.AppConfig.String("sqlconn")+"?charset=utf8")
+	orm.RegisterModel(
+		new(models.User),
+	)
+	err := orm.RunSyncdb("default", false, true)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
