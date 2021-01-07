961d8cd6597d99ef854171bb138c1cb1d6d86076
junyaUch <junya19991215@icloud.com>
2021-01-07 14:52:31 +0900

    JWTによる認証API作成

--- controllers/user.go
+++ controllers/user.go
@@ -1,9 +1,12 @@
 package controllers
 
 import (
-	"log"
+	"NativeAppApi/models"
+	"time"
 
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type UserController struct {
@@ -14,11 +17,46 @@ type aaa struct {
 	Name string
 }
 
-func (this *UserController) Test() {
-	log.Println("Hello")
-	a := aaa{}
-	a.Name = "じゅんや"
+func (this *UserController) CheckUsenameExists() {
+	userName := this.Ctx.Input.Param(":userName")
+	o := orm.NewOrm()
+	user := models.User{UserName: userName}
 
-	this.Data["json"] = a
+	if success := o.Read(&user, "UserName"); success == nil {
+		return
+	}
+
+	this.Data["json"] = true
+	this.ServeJSON()
+}
+
+func (this *UserController) CreateNewUser() {
+	userName := this.GetString("userName")
+	phoneNum := this.GetString("phoneNum")
+	password := this.GetString("password")
+	birthday := this.GetString("birthday")
+
+	//パスワードハッシュ化する
+	hash, _ := bcrypt.GenerateFromPassword([]byte(password), 10)
+	hashPassword := string(hash)
+
+	//必ず山岳部標準時にする必要がある
+	layout := "2006-01-02"
+	parseBarthday, _ := time.Parse(layout, birthday)
+
+	user := models.User{}
+	user.UserName = userName
+	user.PhoneNumber = phoneNum
+	user.Password = hashPassword
+	user.Birthday = parseBarthday
+
+	o := orm.NewOrm()
+	_, err := o.Insert(&user)
+	if err != nil {
+		return
+	}
+
+	jwtToken := IssueJWT(userName, phoneNum, parseBarthday)
+	this.Data["json"] = jwtToken
 	this.ServeJSON()
 }
--- go.mod
+++ go.mod
@@ -6,7 +6,8 @@ require (
 	github.com/astaxie/beego v1.12.3
 	github.com/auth0/go-jwt-middleware v0.0.0-20201030150249-d783b5c46b39 // indirect
 	github.com/aws/aws-sdk-go v1.36.19
-	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/go-sql-driver/mysql v1.5.0
 	github.com/icza/session v1.2.0
+	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9
 )
